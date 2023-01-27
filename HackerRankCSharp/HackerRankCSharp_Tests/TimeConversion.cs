e20c4fc60fe370347251a1b837bf502aeebacde3
Daniel Suarez <dsuarez@nimtech.com.mx>
2023-01-26 19:34:33 -0600

    Added Time Conversion Since it was allready submitted.

---
+++ HackerRankCSharp/HackerRankCSharp_Tests/TimeConversion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCSharp_Tests
+{
+    public class TimeConversion
+    {
+        public static string timeConversion(string s)
+        {
+            var date = DateTime.Parse(s);
+            return date.ToString("HH:mm:ss");
+        }
+    }
+}
